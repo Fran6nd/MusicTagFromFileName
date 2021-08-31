597fae6b78d948a279efd6c026c235c01036436e
Fran6nd <francoisnd1@gmail.com>
2021-08-31 13:44:33 +0200

    wip

--- SetMusicTagsFromFilename.py
+++ SetMusicTagsFromFilename.py
@@ -1,26 +1,26 @@
+#!/usr/local/bin/python3
 import os
+import sys
 import eyed3
 
-directory = os.fsencode("./Spare/")
-    
-for file in os.listdir(directory):
-     filename = os.fsdecode(file)
-     if filename.endswith(".mp3"): 
-        audio = eyed3.load("./Spare/" + filename)
-        #audio.initTag()
-        name = filename[:len(filename) - 4]
-        name = name.split(" - ")
-        print(name)
-        if(len(name) == 1):
-            audio.tag.title = name[0]
-        elif (len(name) == 2):
-            audio.tag.title = name[1]
-            audio.tag.artist = name[0]
-        elif (len(name) == 3):
-            audio.tag.title = name[1]
-            audio.tag.artist = name[0]
-            audio.tag.album = name[2]
-        audio.tag.save("./Spare/" + filename )
-        continue
-     else:
-        continue
+
+
+
+filename = os.fsdecode(sys.argv[1])
+if filename.endswith(".mp3"): 
+    audio = eyed3.load(sys.argv[1])
+    #audio.initTag()
+    filename = os.path.basename(filename)
+    name = filename[:len(filename) - 4]
+    name = name.split(" - ")
+    print(name)
+    if(len(name) == 1):
+        audio.tag.title = name[0]
+    elif (len(name) == 2):
+        audio.tag.title = name[1]
+        audio.tag.artist = name[0]
+    elif (len(name) == 3):
+        audio.tag.title = name[1]
+        audio.tag.artist = name[0]
+        audio.tag.album = name[2]
+    audio.tag.save(sys.argv[1])
