002b19dd8a4ac6a6c428de1f83b5470632678b3a
Fran6nd <francoisnd1@gmail.com>
2021-08-31 14:08:15 +0200

    wip: need cleaning

--- SetMusicTagsFromFilename.py
+++ SetMusicTagsFromFilename.py
@@ -8,7 +8,29 @@ import eyed3
 
 filename = os.fsdecode(sys.argv[1])
 if os.path.isdir(filename):
-    pass
+    directory = filename
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".mp3"): 
+            audio = eyed3.load( directory + "/" + filename)
+            #audio.initTag()
+            name = filename[:len(filename) - 4]
+            name = name.split(" - ")
+            print(name)
+            if(len(name) == 1):
+                audio.tag.title = name[0]
+            elif (len(name) == 2):
+                audio.tag.title = name[1]
+                audio.tag.artist = name[0]
+            elif (len(name) == 3):
+                audio.tag.title = name[1]
+                audio.tag.artist = name[0]
+                audio.tag.album = name[2]
+            audio.tag.save( directory + "/" + filename)
+            continue
+        else:
+            continue
+
 elif filename.endswith(".mp3"): 
     audio = eyed3.load(sys.argv[1])
     #audio.initTag()
